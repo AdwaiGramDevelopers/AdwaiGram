91e8cf470b28e525cf095c352ad90a237e4edd79
Sk7Str1p3 <finenkofedor@gmail.com>
2025-08-13 17:08:36 +0000

    split code

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,9 @@
+mod ui;
+mod constants;
+
 use adw::prelude::*;
 use relm4::prelude::*;
-
-struct AboutPage;
-
-#[relm4::component]
-impl SimpleComponent for AboutPage {
-    type Init = adw::Dialog;
-    type Input = ();
-    type Output = ();
-
-    view! {
-        adw::AboutDialog {
-            // FIXME: relm4-icons bundles wrong icon, or render it in bad way
-            set_application_icon: "logo-devel",
-            set_application_name: "AdwaiGram",
-            set_developer_name: "AdwaiGram developers",
-            set_version: "0.0.1-alpha1",
-            set_website: "https://github.com/AdwaiGramDevelopers/AdwaiGram/",
-            set_support_url: "https://t.me/paperplanechat",
-            set_issue_url: "https://github.com/AdwaiGramDevelopers/AdwaiGram/issues",
-            set_developers: &["Finenko Fedor https://github.com/Sk7Str1p3"],
-            set_artists: &[
-                "Mateus Santos https://github.com/swyknox",
-                "noëlle https://github.com/jannuary",
-            ],
-        }
-    }
-
-    fn init(
-        parent: Self::Init,
-        root: Self::Root,
-        _sender: ComponentSender<Self>,
-    ) -> ComponentParts<Self> {
-        let model = AboutPage;
-        let widgets = view_output!();
-        // Сразу показываем диалог при инициализации
-        root.present(Some(&parent));
-        ComponentParts { model, widgets }
-    }
-}
+use ui::{about::AboutPage, icons};
 
 #[derive(Debug)]
 struct MainWindow;
@@ -57,9 +22,8 @@ impl SimpleComponent for MainWindow {
             gtk::Button {
                 set_label: "About",
                 connect_clicked => move |_| {
-                    // Запускаем компонент AboutPage с передачей текущего окна
                     let parent = adw::Dialog::default();
-                    parent.set_visible(true); // чтобы было куда крепить
+                    parent.set_visible(true);
                     let _ = AboutPage::builder().launch(parent);
                 }
             }
@@ -67,29 +31,16 @@ impl SimpleComponent for MainWindow {
     }
 
     fn init(_init: (), root: Self::Root, _sender: ComponentSender<Self>) -> ComponentParts<Self> {
-        let model = MainWindow;
-        let widgets = view_output!();
+        let model: MainWindow = MainWindow;
+        let widgets: MainWindowWidgets = view_output!();
         ComponentParts { model, widgets }
     }
 }
 
-mod icons {
-    include!(concat!(env!("OUT_DIR"), "/icons.rs"));
-}
-
-fn initialize_custom_icons() {
-    gtk::gio::resources_register_include!("icons.gresource").unwrap();
-
-    let display = gtk::gdk::Display::default().unwrap();
-    let theme = gtk::IconTheme::for_display(&display);
-    theme.add_resource_path("/app/AdwaiGramDevelopers/AdwaiGram/icons");
-}
-
 fn main() {
-    let app = RelmApp::new("app.AdwaiGramDevelopers.AdwaiGram");
+    let app: RelmApp<()> = RelmApp::new(constants::APP_ID);
 
-    //relm4_icons::initialize_icons(icons::GRESOURCE_BYTES, icons::RESOURCE_PREFIX);
-    initialize_custom_icons();
+    relm4_icons::initialize_icons(icons::GRESOURCE_BYTES, icons::RESOURCE_PREFIX);
 
     app.run::<MainWindow>(());
 }
