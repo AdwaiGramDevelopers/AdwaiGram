41d088b23567ae94c132abaa59d6c2054442474b
Sk7Str1p3 <finenkofedor@gmail.com>
2025-08-22 20:05:07 +0000

    purge build script

--- build.rs
+++ build.rs
@@ -1,20 +1,11 @@
 #[path = "data/__build.rs"]
 mod data;
-#[path = "src/__build.rs"]
-mod src;
 
 use std::env::var;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let out_dir = PathBuf::from(var("OUT_DIR").unwrap());
-    let mut app_id = "app.adwaigramdevs.adwaigram".to_string();
-    let profile = var("PROFILE").unwrap();
-
-    src::version::build(&profile, &mut app_id, &out_dir)?;
-    src::api::build(&out_dir)?;
-
-    data::build(&app_id);
+    data::build();
 
     Ok(())
 }
